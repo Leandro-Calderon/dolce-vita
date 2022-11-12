6b5d8d9277e44ec823ab2aede3049afac3bc6f39
Leandro Calderón <mellyleo17@gmail.com>
2022-11-12 11:32:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Header } from "./components/Header";
+import { Principal } from "./components/Principal";
+
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Principal />
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="header_container">
+      <h1>ESTE ES EL HEADER</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
+        quibusdam? HEADER HEADER sunt enim praesentium totam laboriosam quas quia quo
+        aut nihil expedita, incidunt cum ratione dolorem mollitia ut dicta
+        blanditiis!
+      </p>
+    </div>
+  );
+};
